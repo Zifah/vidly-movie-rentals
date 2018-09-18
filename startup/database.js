b0193e693b792bef6314e9ab6941f05f7c07bb90
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-18 09:46:50 +0100

    Initial Commit

---
+++ startup/database.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const winston = require("winston");
+const config = require("config");
+
+module.exports = function connectToDatabase() {
+  const db = config.get("db");
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => winston.info(`Connected to ${db} successfully...`));
+};
